f431c9b2370e17f1a4d557c9dface78f74f1ae29
Mamadou <mamadou.ndiaye@oca.eu>
2023-08-02 09:35:31 +0200

    create a mocked sphere input generation file

--- aplc_optimization/Inputs_Generation/SPHERE_inputs_Generation.py
+++ aplc_optimization/Inputs_Generation/SPHERE_inputs_Generation.py
@@ -9,7 +9,7 @@ _default_pupil_array_size = 8 * u.m
 
 
 def SPHERE_inputs_gen(input_files_dict):
-    """Generate the GPI Primary and Lyot stop(s) for the APLC coronagraph and write to file.
+    """Generate the SPHERE Primary and Lyot stop(s) for the APLC coronagraph and write to file.
         
     Parameters
     ----------
@@ -26,11 +26,7 @@ def SPHERE_inputs_gen(input_files_dict):
 
     filepath = input_files_dict['directory']
     N = input_files_dict['N']
-    #lyot_mask = input_files_dict['lyot_stop']['lyot_mask']
-    #ls_tabs = input_files_dict['lyot_stop']['ls_tabs']
-    #ls_spid = input_files_dict['lyot_stop']['ls_spid']
-    ls_sym = input_files_dict['lyot_stop']['ls_sym']
-    #ap_spid = input_files_dict['aperture']['ap_spid']
+    ls_multi = input_files_dict['lyot_stop']['ls_multi']
     ap_sym = input_files_dict['aperture']['ap_sym']
     pupil_name = input_files_dict['aperture']['pupil_name']
 
@@ -39,25 +35,54 @@ def SPHERE_inputs_gen(input_files_dict):
     else:
         raise TypeError(f'pupil is expected to be symmetric')
 
-    if ls_sym:
-        ls_filename = filepath + f'pupil={pupil_name}_nPup={N}.fits'
-    else:
-        ls_filename = filepath + f'sphere_stop_ST_ALC2_nPup{N:04d}.fits'
-
     config = Path('masks/' + pup_filename)
-
     # Check if primary file already exists, otherwise create GPI primary file
     if config.is_file():
         print('{0:s} exists'.format('masks/' + pup_filename))
     else:
         raise TypeError(f'{pup_filename} not found')
 
-    config = Path('masks/' + ls_filename)
-
+    ls_filenames = []
+    ls_filename = filepath + f'sphere_stop_ST_ALC2_nPup{N:04d}.fits'
     # Check if lyot stop file already exists, otherwise create GPI Lyot mask file
+    config = Path('masks/' + ls_filename)
     if config.is_file():
         print('{0:s} exists'.format('masks/' + ls_filename))
     else:
         raise TypeError(f'{ls_filename} not found')
+    ls_filenames.append(ls_filename)
+
+    if ls_multi:
+
+        ls_filename_lr = filepath + f'sphere_stop_ST_ALC2_nPup{N:04d}_lr.fits'
+        # Check if lyot stop file already exists, otherwise create GPI Lyot mask file
+        config = Path('masks/' + ls_filename_lr)
+        if config.is_file():
+            print('{0:s} exists'.format('masks/' + ls_filename))
+        else:
+            raise TypeError(f'{ls_filename} not found')
+        ls_filenames.append(ls_filename_lr)
+
+        ls_filename_ud = filepath + f'sphere_stop_ST_ALC2_nPup{N:04d}_ud.fits'
+        # Check if lyot stop file already exists, otherwise create GPI Lyot mask file
+        config = Path('masks/' + ls_filename_ud)
+        if config.is_file():
+            print('{0:s} exists'.format('masks/' + ls_filename))
+        else:
+            raise TypeError(f'{ls_filename} not found')
+        ls_filenames.append(ls_filename_ud)
+
+        ls_filename_lrud = filepath + f'sphere_stop_ST_ALC2_nPup{N:04d}_lrud.fits'
+        # Check if lyot stop file already exists, otherwise create GPI Lyot mask file
+        config = Path('masks/' + ls_filename_lrud)
+        if config.is_file():
+            print('{0:s} exists'.format('masks/' + ls_filename))
+        else:
+            raise TypeError(f'{ls_filename} not found')
+        ls_filenames.append(ls_filename_lrud) 
+
+    # remove duplicates
+    s = set(ls_filenames)
+    ls_filenames = list(s)
     
-    return pup_filename, ls_filename
+    return pup_filename, ls_filenames
